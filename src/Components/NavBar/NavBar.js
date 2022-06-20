b17cf9c1cab843c59833a465c1412646a5548b49
LuisLCalvi <calvilautaro5@gmail.com>
2022-06-20 19:34:47 -0300

    actualizando nav

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -1,11 +1,13 @@
 import React from 'react'
+import LocalGroceryStoreIcon from '@mui/icons-material/LocalGroceryStore';
+import logo from '../../assets/imagenlogo.jpg'
 
 
 const NavBar = () => {
 return (
     
     <>  
-
+        <img style={style.imagenstyle} src = {logo} alt=""  />
         <h1 style ={style.h2Style}>TIENDA SHOP</h1> 
 
         <nav style={style.navStyle}>
@@ -14,6 +16,8 @@ return (
         <a style={style.anchor} href="">Categoria 2</a>
         <a style={style.anchor} href="">Categoria 3</a>
         <a style={style.anchor} href="">Contacto</a>
+
+        <LocalGroceryStoreIcon color="primary" fontSize="large" />
 
     </nav>
 
@@ -31,6 +35,12 @@ return (
 export default NavBar
 
 const style = {
+
+    imagenstyle:{
+        width: '10%',
+        heigth: '10%',
+        display: 'flex',
+    },
 
     h2Style:{
         display: 'flex',
