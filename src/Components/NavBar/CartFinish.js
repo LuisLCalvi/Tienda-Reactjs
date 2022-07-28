9d4cb3b25c104060cbe330cad0ccba4753a8691c
Luis Lautaro Calvi <calvilautaro5@gmail.com>
2022-07-28 12:11:14 -0300

    modificando item colection II

--- src/Components/NavBar/CartFinish.js
+++ src/Components/NavBar/CartFinish.js
@@ -1,95 +1,60 @@
 import React, { useContext, useState } from "react";
 import { db } from "../../firebase/firebase";
-import{ addDoc, doc, collection, serverTimestamp, getDoc, updateDoc, query, where } from "firebase/firestore"
+import{ addDoc, doc, collection, serverTimestamp, getDoc, updateDoc} from "firebase/firestore"
 import { cartContext } from '../../Context/CartContext';
 import swal from 'sweetalert';
-
-
+import './CartFinish.css'
 const CartFinish = () => {
-    const{ products, clear, totalPrice } = useContext(cartContext);
+
+    const{ products, clear, totalPrice, qtyProducts } = useContext(cartContext);
 
     const [form, setForm] = useState({});
     const [sendProducts, setSendProducts] = useState(false);
-    
+
     const finalizarCompra = () => {
       const ventasCollection = collection(db, 'ventas');
       addDoc(ventasCollection, {
         form,
         items: products,
         date: serverTimestamp(),
-        total: totalPrice,
-        priceFinish: totalPrice * 1.21,
-
-
+        PriceIVA:  totalPrice () * 1.21 , 
       })
-    };
+      .then(({id}) => {
+        console.log(id);
+        let ordenid = id;
+        updateStockDb(products);
+        clear();
+       // enviar link a orden
+       ;
+      })
+      .catch(error => console.err);     
+    
+    }
 
+    const updateStockDb = (products) => {
+      products.forEach((product) => {
+        updateStock(product.id, product.qtyProducts);
+      });      
+    }
 
-
-let orden = {
-    form,
-    items: products,
-    date: serverTimestamp(),
-    total: totalPrice,
-    priceFinish: totalPrice * 1.21,
-
-  };
- // updateCollectiondb(products);
-  clear();
-  //validarStock (result.id, qty);
-  obtenerIdVenta(orden)
-
-
-const obtenerIdVenta = (orden) => {
-    const ventasCollection = collection(db, 'ventas');
-    const q = query(ventasCollection, where('totalFinal', '==', orden.totalFinal), where('IVA', '==', orden.IVA), where('Total', '==', orden.Total),where("form.name", "==", orden.form.name), where("form.lastName", "==", orden.form.lastName), where("form.email", "==", orden.form.email), where("form.address", "==", orden.form.address), where("form.phone", "==", orden.form.phone));  
-
-    getDoc(q)
+    const updateStock = (id, qtyProducts) => {
+      let product;
+      const productCollection = collection(db, 'productos');
+      const referenceDoc = doc(productCollection, id);
+      getDoc(referenceDoc)
       .then(result => {
-        const lista  = result.docs.map(product => {
-          return {
-            id: product.id,
-            ...product.data(), 
-          }
-        })
-        setSendProducts(lista); 
-      }).catch(error => console.err)
-  }
+          product = {
+            id: result.id,
+            stock: result.data().stock - qtyProducts,
+         }
+          updateDoc(referenceDoc, product)
+          product.stock < 0 ? swal("No hay stock suficiente", "Gracias", "Error") : swal("Compra realizada", "Gracias", "success");
+      })
+    } 
 
-//const updateCollectiondb = (products) => {
-    //products.forEach((product) => {
-  //      updateStock(product.id, product.qty);
-  //  });      
- // }
 
-  // const updateStock = (id, quantity) => {
-  //   let product;
-  //   const productCollection = collection(db, 'productos');
-  //   const referenceDoc = doc(productCollection, id);
+    
 
-  //   getDoc(referenceDoc)
-  //   .then(result => {
-  //       product = {
-  //         id: result.id,
-  //         stock: result.data().stock - quantity,
-  //      }
-  //       updateDoc(referenceDoc, product)
-  //   })
-  // } 
-
-  // const validarStock = (id, quantity) => {
-  //   let product;
-  //   const productCollection = collection(db, 'productos');
-  //   const referenceDoc = doc(productCollection, id);
-  //   getDoc(referenceDoc)
-  //   .then(result => {
-  //       product = {
-  //         id: product.id,
-  //         stock: product.data().stock - product.qty,
-  //      }
-  //     product.stock < 0 ? swal("No hay stock suficiente", "Error") : swal("Compra realizada", "Gracias por su compra", "success");
-  //   })
-  // }
 
   const handleChange = (e) => {
     setForm({
@@ -98,7 +63,7 @@ const obtenerIdVenta = (orden) => {
     });
   }
 
-  const handleSubmit = (e) => {
+  const onSubmit = (e) => {
     e.preventDefault();
     finalizarCompra();
     
@@ -108,8 +73,8 @@ const obtenerIdVenta = (orden) => {
     <>
     
     <div className="container">
-      <h3>Formulario de Envio </h3>
-      <form onSubmit={handleSubmit}>
+      <h2 >Formulario de Envio </h2>
+      <form onSubmit={onSubmit}>
         <label htmlFor='Name'>Nombre Completo</label>
         <input type="text" value={form.name} name= "nombre" onChange={handleChange} />
         <label htmlFor='email'>Email</label>
@@ -124,13 +89,14 @@ const obtenerIdVenta = (orden) => {
         <input type="text" value={form.state} name="state" onChange={handleChange} />
         <label htmlFor='city'>Ciudad</label>
         <input type="text" value={form.city} name="city" onChange={handleChange} />
-        <button type="submit">Enviar</button> 
-      </form>
+        <button type="submit" class="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">Save</button>      
+        </form>
       
     </div> 
     </>
   );
 
   };
+
 
 export default CartFinish;
